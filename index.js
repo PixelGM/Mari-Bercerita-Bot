3972f281cbaa8818d89cc9bbb1110644261ec06d
ピクセル GM <35326620+PixelGM@users.noreply.github.com>
2023-03-12 23:42:22 -0700

    No username and enter replaced with space

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ client.on('message', async message => {
   if (message.content === '!story') {
     const messages = await message.channel.fetchMessages({ limit: 100 });
     const userMessages = messages.filter(msg => msg.author.id === message.author.id && !msg.content.toLowerCase().includes('!story'));
-    let story = userMessages.reduce((acc, msg) => `${msg.author.username}: ${msg.content}\n${acc}`, '');
+    let story = userMessages.reduce((acc, msg) => `${msg.content}\n${acc}`, '');
     while (messages.size === 100) {
       const lastMessageId = messages.last().id;
       messages = await message.channel.fetchMessages({ limit: 100, before: lastMessageId });
       const moreUserMessages = messages.filter(msg => msg.author.id === message.author.id && !msg.content.toLowerCase().includes('!story'));
-      story = moreUserMessages.reduce((acc, msg) => `${msg.author.username}: ${msg.content}\n${acc}`, story);
+      story = moreUserMessages.reduce((acc, msg) => `${msg.content}\n${acc}`, story);
     }
     const channel = client.channels.get('1084707829597880330');
-    channel.send(story);
+    channel.send(story.replace(/\n/g, ' '));
   }
 });
 
