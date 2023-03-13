934e79419808317a3fcf6159ff6244b9bd295c66
ピクセル GM <35326620+PixelGM@users.noreply.github.com>
2023-03-13 00:03:58 -0700

    READY TO ROLL BABY! WOHOO~~!

--- index.js
+++ index.js
@@ -8,12 +8,25 @@ client.on('ready', () => {
 client.on('message', async message => {
   if (message.content === '!story') {
     const messages = await message.channel.fetchMessages({ limit: 100 });
-    const userMessages = messages.filter(msg => msg.author.id === message.author.id && !msg.content.toLowerCase().includes('!story'));
+    let stopMessage = null;
+    for (const msg of messages.array().reverse()) {
+      if (msg.content.toLowerCase().includes('stop')) {
+        stopMessage = msg;
+        break;
+      }
+    }
+    let userMessages = messages.filter(msg => !msg.content.toLowerCase().includes('!story'));
+    if (stopMessage !== null && stopMessage.createdAt > userMessages.last().createdAt) {
+      userMessages = userMessages.filter(msg => msg.createdAt > stopMessage.createdAt);
+    }
     let story = userMessages.reduce((acc, msg) => `${msg.content}\n${acc}`, '');
     while (messages.size === 100) {
       const lastMessageId = messages.last().id;
       messages = await message.channel.fetchMessages({ limit: 100, before: lastMessageId });
-      const moreUserMessages = messages.filter(msg => msg.author.id === message.author.id && !msg.content.toLowerCase().includes('!story'));
+      const moreUserMessages = messages.filter(msg => !msg.content.toLowerCase().includes('!story'));
+      if (stopMessage !== null && stopMessage.createdAt > moreUserMessages.last().createdAt) {
+        userMessages = moreUserMessages.filter(msg => msg.createdAt > stopMessage.createdAt);
+      }
       story = moreUserMessages.reduce((acc, msg) => `${msg.content}\n${acc}`, story);
     }
     const channel = client.channels.get('1084707829597880330');
