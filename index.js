9f606e186dde8c122afe855b45df421dd3a007d5
ピクセル GM <35326620+PixelGM@users.noreply.github.com>
2023-03-13 11:12:12 -0700

    Fixed code that outputs twice

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+let outputSent = false;
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', async message => {
-  if (message.content === '!story') {
+  if (message.content === '!story' && !outputSent) {
     let messages = await message.channel.fetchMessages({ limit: 100 });
     let stopMessage = null;
-    for (const msg of messages.array().reverse()) {
+    for (const msg of messages.array()) {
       if (msg.content.toLowerCase().includes('tamat') && !messages.some(m => m.createdAt > msg.createdAt && m.content.toLowerCase().includes('tamat'))) {
         stopMessage = msg;
         break;
@@ -19,20 +21,10 @@ client.on('message', async message => {
     if (stopMessage !== null && stopMessage.createdAt >= userMessages.last().createdAt) {
       userMessages = userMessages.filter(msg => msg.createdAt > stopMessage.createdAt);
     }
-    let story = userMessages.reduce((acc, msg) => `${msg.content}\n${acc}`, '');
-    let lastMessageId = messages.last().id;
-    while (true) {
-      messages = await message.channel.fetchMessages({ limit: 100, before: lastMessageId });
-      if (messages.size === 0) {
-        break;
-      }
-      const moreUserMessages = messages.filter(msg => !msg.content.toLowerCase().includes('!story') && msg.createdAt > stopMessage.createdAt);
-      userMessages = userMessages.concat(moreUserMessages);
-      lastMessageId = messages.last().id;
-    }
-    story = userMessages.reduce((acc, msg) => `${msg.content}\n${acc}`, story);
+    let story = userMessages.filter(msg => !msg.content.toLowerCase().includes('!story')).array().reverse().reduce((acc, msg) => `${acc}${msg.content} `, ''); // add reverse()
     const channel = client.channels.get('836599878218022943');
-    channel.send(story.replace(/\n/g, ' '));
+    channel.send(story);
+    outputSent = true;
   }
 });
 
