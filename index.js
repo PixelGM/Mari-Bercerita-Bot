2c99e69f988affecf5203471fab180c95d55a107
ピクセル GM <35326620+PixelGM@users.noreply.github.com>
2023-03-12 23:39:20 -0700

    First Work!

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on('message', async message => {
+  if (message.content === '!story') {
+    const messages = await message.channel.fetchMessages({ limit: 100 });
+    const userMessages = messages.filter(msg => msg.author.id === message.author.id && !msg.content.toLowerCase().includes('!story'));
+    let story = userMessages.reduce((acc, msg) => `${msg.author.username}: ${msg.content}\n${acc}`, '');
+    while (messages.size === 100) {
+      const lastMessageId = messages.last().id;
+      messages = await message.channel.fetchMessages({ limit: 100, before: lastMessageId });
+      const moreUserMessages = messages.filter(msg => msg.author.id === message.author.id && !msg.content.toLowerCase().includes('!story'));
+      story = moreUserMessages.reduce((acc, msg) => `${msg.author.username}: ${msg.content}\n${acc}`, story);
+    }
+    const channel = client.channels.get('1084707829597880330');
+    channel.send(story);
   }
 });
 
